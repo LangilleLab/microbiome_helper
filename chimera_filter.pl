ce4535a06c28d4372af964cae4cf6befc7735e86
Gavin Douglas <gavin@kronos.pharmacology.dal.ca>
2016-05-27 12:42:08 -0300

    updated chimera_filter.pl script so that it wraps vsearch rather than usearch (same base algorithm)

--- chimera_filter.pl
+++ chimera_filter.pl
@@ -2,13 +2,14 @@
 
 use warnings;
 use strict;
+
 use Parallel::ForkManager;
 use File::Basename;
 use Getopt::Long;
 use Pod::Usage;
 
 my $help; 
-my $version = "1.0";
+my $version = "2.0";
 my $version_marker;
 
 my $parallel;
@@ -17,7 +18,9 @@ my $out_dir = "non_chimeras";
 my $log = "chimeraFilter_log.txt";
 my $type;
 my $mindiv = 1.5;
-my $minh = 0.2;
+my $minh = 0.2; 
+my $keep;  
+my @log_files = (); ### array to contain all log files, which will be looped over at the end
 
 my $res = GetOptions("type:i"=> \$type,
 		     "out_dir|o=s" => \$out_dir,
@@ -26,6 +29,7 @@ my $res = GetOptions("type:i"=> \$type,
 		     "thread:i"=>\$parallel,
 		     "mindiv=f"=>\$mindiv,
 		     "minh=f"=>\$minh,
+		     "keep"=>\$keep,
 		     "help|h"=>\$help,
 		     "version|v"=>\$version_marker,
 	  )	or pod2usage(2);
@@ -39,139 +43,132 @@ my $cpu_count=1;
 if(defined($parallel)){
     #option is set but with no value then use the max number of proccessors
     if($parallel ==0){
-	#load this module dynamically
-	eval("use Sys::CPU;");
-	$cpu_count=Sys::CPU::cpu_count();
-    }else{
-	$cpu_count=$parallel; 
+		#load this module dynamically
+		eval("use Sys::CPU;");
+		$cpu_count=Sys::CPU::cpu_count();
+    } else {
+		$cpu_count=$parallel; 
     }
 }
 
-if ( index( `usearch61 --help` , "Edgar" ) == -1 )	{	die "usearch61 is not in your path. You might not have the right usearch version and/or the binary might not be named \"usearch61\"\n";	}
+### the "2>&1" syntax means output STDERR to same output as STDOUT (i.e. same output as file descripter 1).
+if ( index( `vsearch 2>&1` , "torognes/vsearch" ) == -1 )      { die "Stopping job, because \"vsearch\" is not in your path.\n";	} 	
 
-if ( ( ! defined $type ) or (  ( $type != 0 ) and ( $type != 1 ) ) )	{	die "output type \"-type\" needs to be either 1 (only clear non-chimeras) or 0 (any non-chimera, which includes those which are borderline)\n";	}
+if ( ( ! defined $type ) or (  ( $type != 0 ) and ( $type != 1 ) ) )	{	die "output type \"-type\" needs to be either 1 (only clear non-chimeras) or 0 (any non-chimera, which includes those which are borderline)\n\nType \"chimeraFilter.pl -h\" for help.\n\n";	}
 
-if ( -e $db )	{	} else { die "database file $db not found\n";	}
-
-my @files=@ARGV;
-
+my @files = @ARGV;
 pod2usage($0.': You must provide a list of fasta files to be filtered.') unless @files;
 if ( ! defined $db )	{	die "need database file\n";	}
-
-open( 'MASTERLOG' , '>' , $log ) or die "cant create MASTERLOG file $log\n";
-print MASTERLOG "file	chimeraCalls	unclearCalls	nonChimeraCalls	chimeraCallsPercent	unclearCallsPercent	nonChimeraCallsPercent\n";
 
 system( "mkdir -p $out_dir");
 
 my $pm = new Parallel::ForkManager($cpu_count);
 
 foreach my $f ( @files )	{
-		
-	$pm->start and next;
 	
 	my $base = basename( $f );
+	
+	### store log files to loop through at the end:
 	my $err = $out_dir ."/".$base . ".LOG";
+	push( @log_files , $err );
+	
+	$pm->start and next;
 	
 	my @baseSplit = split( '\.' , $base );
-
+	
 	my $ext = pop @baseSplit;
 	my $out = join( "." , @baseSplit ) . ".nonchimera.".$ext;
-
+	my $unclear_out = join( "." , @baseSplit ) . ".unclear.".$ext;
+	
 	$out = "$out_dir" ."/".$out;
+	$unclear_out = "$out_dir" ."/". $unclear_out;
 	
-	my $uchimeOutTmp = $out . ".uchimeTMP.txt";
-			
-	print STDERR "usearch61 -uchime_ref $f -db $db  -minh $minh -mindiv $mindiv -threads 1 -strand plus -uchimeout $uchimeOutTmp 2>$err\n";
-	
-	system( "usearch61 -uchime_ref $f -db $db  -minh $minh -mindiv $mindiv -threads 1 -strand plus -uchimeout $uchimeOutTmp 2>$err >>uchime_tmp.txt" );
+	if ( $type == 0 )	{	
 		
-	my %calls = ();
-	my %counts = ();
-
-	$counts{"Y"} = 0;
-	$counts{"N"} = 0;
-	$counts{"?"} = 0;
-
-	open( 'UCHIME' , '<' , $uchimeOutTmp ) or die "cant open UCHIME $uchimeOutTmp\n";
-	while (<UCHIME> )	{
-		my @split = split( '\s+' , $_ );
-		$calls{$split[1]} = $split[$#split];
-		$counts{$split[$#split]} += 1;
-	} close( 'UCHIME' );
-
-
-	open( 'OUT' , '>' , $out ) or die "cant create OUT file $out\n";
-	open ( 'IN' , '<' , $f ) or die "cant open IN file $f\n";
-
-	my $next = 0;
-
-	while ( <IN> )	{
+		print STDERR "vsearch --uchime_ref $f --db $db --minh $minh --mindiv $mindiv --threads 1 --nonchimeras $out --borderline $unclear_out >> tmp_vsearch_out.txt  2> $err\n"; 
+		system( "vsearch --uchime_ref $f --db $db --minh $minh --mindiv $mindiv --threads 1 --nonchimeras $out --borderline $unclear_out >> tmp_vsearch_out.txt  2> $err" );
 		
-		my @split = split( '\s+' , $_ );
-		if ( ! exists $split[0] )	{	next	};
-	
-		my $first = substr( $_ , 0 , 1 );
-
-		if ( $first eq ">" )	{
+		my $any_non_chimera = "$out_dir" . "/" . join( "." , @baseSplit ) . ".nonchimera_and_unclear." . $ext;		
 		
-			my $name = substr( $split[0] , 1 );
-			
-			if ( ! exists $calls{$name} )	{	die "sequence $name not found in uchime output. Did uchime run properly?\n";	}		
+		### combine clear non-chimeras and borderline sequences:
+		print STDERR "cat $out $unclear_out > $any_non_chimera\n";
+		system( "cat $out $unclear_out > $any_non_chimera" );	
+		
+		if ( ! $keep )	{
+			### remove temp out files:
+			print STDERR "rm $out\n";
+			system( "rm $out" );
+			print STDERR "rm $unclear_out\n";
+			system( "rm $unclear_out" );
+		}
+		
+	} elsif ( $type == 1 )	{
 	
-			if ( $calls{$name} eq "N" )	{
-				print OUT "$_";	
-				$next = 1;	
-			} elsif ( ( $calls{$name} eq "?" ) and ( $type == 0 ) )	{
-				print OUT "$_";
-				$next = 1;	
-			} else {
-				$next = 0;
-			}
-		} elsif ( $next == 1 )	{
-			print OUT "$_";
-		} else {}
-	} close ('IN' );
-	close( 'OUT' );
+		print STDERR "vsearch --uchime_ref $f --db $db --minh $minh --mindiv $mindiv --threads 1 --nonchimeras $out >> tmp_vsearch_out.txt  2> $err\n"; 
+		system( "vsearch --uchime_ref $f --db $db --minh $minh --mindiv $mindiv --threads 1 --nonchimeras $out >> tmp_vsearch_out.txt  2> $err" );
 
-	my $chimeras = $counts{"Y"};
-	my $non = $counts{"N"};
-	my $unclear = $counts{"?"};
-
-	my $total = $chimeras + $non + $unclear;
-
-	if ( $total == 0 )	{	die "no sequences in uchime output file... stopping\n";	}
-
-	my $perChimera = ( $chimeras / $total) * 100;
-	my $perNon = ( $non / $total ) * 100;
-	my $perUnclear = ( $unclear / $total) * 100;
-	
-	$perChimera = sprintf ( "%.1f" , $perChimera );
-	$perNon = sprintf ( "%.1f" , $perNon );
-	$perUnclear = sprintf ( "%.1f" , $perUnclear );
-
-	print MASTERLOG "$base	$chimeras	$unclear	$non	$perChimera	$perUnclear	$perNon\n";
-
-	system( "rm $uchimeOutTmp" );
-	system( "rm $err" );
+	}
 	
 	$pm->finish;
 }
 
 $pm->wait_all_children;
 
-system( "rm uchime_tmp.txt");
+### create single log file for all samples by looping through individual log files
+open( 'MASTERLOG' , '>' , $log ) or die "cant create MASTERLOG file $log\n";
+print MASTERLOG "file	chimeraCalls	unclearCalls	nonChimeraCalls	chimeraCallsPercent	unclearCallsPercent	nonChimeraCallsPercent\n";
+
+my $i = 0;
+
+foreach my $err ( @log_files )	{
+
+	### read in log file for vsearch job:
+	open( 'LOG' , '<' , $err ) or die "cant open LOG $err\n";
+	chomp( my @lines = <LOG> );
+	close( 'LOG' );
+	
+	my $base = basename( $files[$i] );
+	
+	### scan 2nd last line of file for info on chimeras and non-chimeras:
+	$lines[$#lines-1] =~ m/(\d+) \((\S+)%\) chimeras, (\d+) \((\S+)%\) non-chimeras,/;
+	my $chimeras = $1;
+	my $perChimera = $2;
+	my $non = $3;
+	my $perNon = $4;
+	
+	### scan last line of file for info on borderline (i.e. unclear) sequences:
+	$lines[$#lines] =~ m/and (\d+) \((\S+)%\) borderline sequences in (\d+) total sequences\./;
+	my $unclear = $1;
+	my $perUnclear = $2;
+	
+	print MASTERLOG "$base	$chimeras	$unclear	$non	$perChimera	$perUnclear	$perNon\n";
+
+	### remove individual log files for each vsearch job:
+	if ( ! $keep )	{	system( "rm $err" );	}
+	++$i;
+}
+
+close( 'MASTERLOG' );
+system( "rm tmp_vsearch_out.txt" );
 
 =head1 Name
 
-chimeraFilter.pl - wrapper to filter out chimeric reads from fasta files (using usearch 6.1, specifically uchime).
+chimeraFilter.pl - wrapper to filter out chimeric reads from fasta files (with vsearch, which uses the uchime algorithm).
 
 
 =head1 USAGE
 
-chimeraFilter.pl [-log <logfile> -thread <#_CPU_to_use> -o <out_dir> -minh <minimum chimera score> -mindiv <minimum divergence> -h -v]  -type <0 or 1> -db <database of known 16S genes> <list of fasta files>
+chimeraFilter.pl [-log <logfile> -thread <#_CPU_to_use> -o <out_dir> -minh <minimum chimera score> -mindiv <minimum divergence> -h -v --keep]  -type <0 or 1> -db <FASTA> <list of FASTA files>
 
 
-NOTE: currently the binary "usearch61" (USEARCH 6.1) needs to be in your path, you may need to rename the default binary to usearch61. This will be fixed soon!
+NOTE: the "vsearch" binary needs to be in your PATH.
+
+vsearch GitHub page: https://github.com/torognes/vsearch/  
+
+uchime paper: http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3150044/
+
+uchime algorithm: http://drive5.com/usearch/manual/uchime_algo.html
+
 
 =head1 OPTIONS
 
@@ -191,16 +188,16 @@ Non-chimeric output type, either only sequences that are clearly non-chimeric (1
 
 or
 
-all sequences that are not called as chimeric ( 0 - includes borderline sequences, "?" in uchime output).
+all sequences that are not called as chimeric ( 0 - includes borderline sequences).
 
 
 =item B<-mindiv <float>>
 
-Min % divergence between query and target sequence (default 1.5, note that this differs from the uchime default of 0.8).
+Min % divergence between query and target sequence (default 1.5, note that this differs from the vsearch default of 0.8).
 
 =item B<-minh <float>>
 
-Min score to be called as chimeric (default 0.2, note that this differs from the uchime default of 0.28).
+Min score to be called as chimeric (default 0.2, note that this differs from the vsearch default of 0.28).
 
 =item B<-o, --out_dir <file>>
 
@@ -210,13 +207,17 @@ Output directory for filtered fastq files. Default is "non_chimeras".
 
 Using this option without a value will use all CPUs on machine, while giving it a value will limit to that many CPUs. Without option only one CPU is used. 
 
+=item B<-keep>
+
+Flag to indicate that temporary log files should not be removed (useful for troubleshooting). Also, will prevent the "nonchimera" and "unclear" specific fastas from being removed when type == 0. 
+
 =item B<-log <file>>
 
 The location to write the log file.
  
 =item B<-db, --database <file>> 
 
-Database of 16S sequences to use as a reference (UDB or FASTA file). See http://www.drive5.com/usearch/manual/udb_files.html for more details on UDB files.
+Database of 16S sequences to use as a reference (FASTA file).
 
 =back
 
